0d2ad16fbab2a53524665249a28f9e79c511930c
Ivan Kunz <thecrazymonkey@users.noreply.github.com>
2022-03-16 21:52:49 -0400

    tried for more resiliency

--- app.py
+++ app.py
@@ -47,43 +47,47 @@ def info():
 @app.route("/metrics")
 def metrics():
     devices = get_all_devices()
-    device_attributes = []
-    for device in devices.json():
-        for attrib in device['attributes']:
-            value = device['attributes'][attrib]
-            # Is this a metric we should be collecting?
-            if attrib in collected_metrics:
-                # Does it have a "proper" value?
-                if value is not None:
-                    # If it's a switch, then change from text to binary values
-                    if attrib == "switch":
-                        if value  == "on":
-                            value = 1
-                        else:
-                            value  = 0
-                    if attrib == "power":
-                        if value  == "on":
-                            value  = 1
-                        elif value == "off":
-                            value = 0
+    if devices.status_code == 200:
+        device_attributes = []
+        for device in devices.json():
+            for attrib in device['attributes']:
+                value = device['attributes'][attrib]
+                # Is this a metric we should be collecting?
+                if attrib in collected_metrics:
+                    # Does it have a "proper" value?
+                    if value is not None:
+                        # If it's a switch, then change from text to binary values
+                        if attrib == "switch":
+                            if value  == "on":
+                                value = 1
+                            else:
+                                value  = 0
+                        if attrib == "power":
+                            if value  == "on":
+                                value  = 1
+                            elif value == "off":
+                                value = 0
 
-                    # Sanitise the device name as it will appear in the label
-                    device_name = device['label'].lower().replace(' ','_').replace('-','_')
-                    # Sanitise the metric name 
-                    metric_name = attrib.lower().replace(' ','_').replace('-','_')
-                    # Create the dict that holds the data
-                    device_attributes.append({
-                        "device_name": f"{device_name}",
-                        "metric_name": f"{metric_name}",
-                        "metric_value": f"{value}",
-                        "metric_timestamp": time.time()})
-    # Create the response
-    response = make_response(render_template('base.txt',
-            device_details=device_attributes
-            ))
-    # Make sure we return plain text otherwise Prometheus complains
-    response.mimetype = "text/plain"
+                        # Sanitise the device name as it will appear in the label
+                        device_name = device['label'].lower().replace(' ','_').replace('-','_')
+                        # Sanitise the metric name 
+                        metric_name = attrib.lower().replace(' ','_').replace('-','_')
+                        # Create the dict that holds the data
+                        device_attributes.append({
+                            "device_name": f"{device_name}",
+                            "metric_name": f"{metric_name}",
+                            "metric_value": f"{value}",
+                            "metric_timestamp": time.time()})
+        # Create the response
+        response = make_response(render_template('base.txt',
+                device_details=device_attributes
+                ))
+        # Make sure we return plain text otherwise Prometheus complains
+        response.mimetype = "text/plain"
+    else:
+        response = devices
     return response
+
 
 
 
